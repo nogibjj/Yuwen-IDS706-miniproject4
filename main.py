ce4240e7d97215f6e09b71a1038964460ee907d7
Candice1121 <75481901+Candice1121@users.noreply.github.com>
2023-09-20 14:57:39 -0400

    Update with python matrix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def load_data(datapath):
     return pd.read_csv(datapath)
@@ -12,3 +13,13 @@ def get_data_descriptive_stats(dataframe):
         'Max': dataframe.max(numeric_only=True)
     }
     return pd.Series(statistics)
+
+def plot_hist(dataframe, col):
+    """Plot histogram of the given column"""
+    plt.figure(figsize=(3.1104,2.0736))
+    plt.hist(dataframe[col], bins=10, color='green', edgecolor='black', linewidth=1.2)
+    plt.title(f"Histogram of {col}")
+    plt.xlabel(col)
+    plt.ylabel("Frequency")
+    visualization_path = 'output/visualization_hist.png'
+    plt.savefig(visualization_path)  # save png
--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
-from main import load_data, get_data_descriptive_stats
+from main import load_data, get_data_descriptive_stats, plot_hist
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def test_loaddata():
     #load iris dataset
@@ -15,3 +16,9 @@ def test_descriptive_stats():
     assert statistics['Mean']['sepal_length'] == 5.843333333333334
 
 
+def test_plot_hisogram():
+    path = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv'
+    iris_df = load_data(path) 
+    plot_hist(iris_df, 'sepal_length')
+    fig = plt.gcf()
+    assert len(fig.axes) > 0
